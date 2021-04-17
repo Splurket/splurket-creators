0532fd0c0c9ebf6be5605885a34e11e0b366dbc9
Dylan Clayton <dylanclayton3@gmail.com>
2021-04-17 19:00:13 -0400

    commit

--- js/main.js
+++ js/main.js
@@ -144,3 +144,10 @@
 	
  
 })(jQuery); 
+function productshow() {
+	var all = document.getElementsByClassName('dashboard-sex');
+    for (var i = 0; i < all.length; i++) {
+      all[i].style.display = 'none';
+    }
+
+}
