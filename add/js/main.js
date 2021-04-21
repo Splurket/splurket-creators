9fca8e1e79afc7d929c54b44697e2182b6f8052f
Dylan Clayton <dylanclayton3@gmail.com>
2021-04-21 15:44:14 -0400

    commit

---
+++ add/js/main.js
@@ -0,0 +1,24 @@
+function Addproduct() {
+	document.getElementById('name').value;
+
+	document.getElementById('price').value;
+
+	document.getElementById('category').value;
+
+	document.getElementById('subcategory').value;
+
+	document.getElementById('cover-drop').value;
+
+	document.getElementById('fileInput').vlaue
+
+	document.getElementById('DownloadLink').value;
+
+	document.getElementById('productupload').value;
+
+	document.getElementById('shipping-selection').value;
+
+	document.getElementById('shipping-template').value;
+
+	document.getElementById('templateupload').value;
+
+}
