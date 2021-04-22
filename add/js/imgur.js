a8ffbb4c1bd32bb3cfd9dfe24fe598a47ede31f2
Dylan Clayton <dylanclayton3@gmail.com>
2021-04-22 10:09:48 -0400

    commit

--- add/js/imgur.js
+++ add/js/imgur.js
@@ -93,10 +93,8 @@
 
             div = this.createEls('div', {className: 'loading-modal'});
             table = this.createEls('table', {className: 'loading-table'});
-            img = this.createEls('img', {className: 'loading-image', src: './css/loading-spin.svg'});
 
             div.appendChild(table);
-            table.appendChild(img);
             document.body.appendChild(div);
         },
         status: function (el) {
