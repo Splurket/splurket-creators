5116378a5a0a7ed94fbaadaa6290a816bf1d3196
Dylan Clayton <dylanclayton3@gmail.com>
2021-04-22 23:26:50 -0400

    commit

--- vuescript.js
+++ vuescript.js
@@ -95,6 +95,7 @@ var firebaseConfig = {
       });
     })
     })
+    })
     });
 
 
@@ -159,5 +160,4 @@ var firebaseConfig = {
             this.pagination.descending = false;
           }
         } } });
-  });
   
