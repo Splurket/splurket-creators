7ed26215f29030086b85d077e2df6c0a4cd9d7f3
Dylan Clayton <dylanclayton3@gmail.com>
2021-04-23 16:48:29 -0400

    commit

--- vuescript.js
+++ vuescript.js
@@ -53,15 +53,7 @@ var firebaseConfig = {
 
             if (doc.exists) {
               var doc= doc.data();
-              var push_data1 = `{
-              value: false,
-              name:${doc.product_name},
-              Price: '${doc.product_price}',
-              Date_Added: '${doc.creation_date}',
-              reviews: '${doc.reviews}',
-              purchases: '${doc.purchases}',
-              status: '${doc.status}'
-            }`
+              var push_data1 = `{value: false, name:${doc.product_name}, Price: '${doc.product_price}', Date_Added: '${doc.creation_date}', reviews: '${doc.reviews}',purchases: '${doc.purchases}',status: '${doc.status}'}`
               if (product_data.includes('{')){
                 var push_data = ','+push_data1;
                 product_data.push(JSON.stringify(push_data))
