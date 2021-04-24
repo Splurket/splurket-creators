be89722e839c39c20ac1237ffacba6ecac068f7e
Dylan Clayton <dylanclayton3@gmail.com>
2021-04-24 07:33:33 -0400

    commit

--- vuescript.js
+++ vuescript.js
@@ -150,47 +150,6 @@ var firebaseConfig = {
           } else {
             this.pagination.sortBy = column;
             this.pagination.descending = false;
-          }}
-        editItem(item) {
-          this.editedIndex = this.products.indexOf(item);
-          this.editedItem = Object.assign({}, item);
-          this.dialog = true;
-        },
-
-        deleteItem(item) {
-          this.editedIndex = this.products.indexOf(item);
-          this.editedItem = Object.assign({}, item);
-          this.dialogDelete = true;
-        },
-
-        deleteItemConfirm() {
-          this.products.splice(this.editedIndex, 1);
-          this.closeDelete();
-        },
-
-        close() {
-          this.dialog = false;
-          this.$nextTick(() => {
-            this.editedItem = Object.assign({}, this.defaultItem);
-            this.editedIndex = -1;
-          });
-        },
-
-        closeDelete() {
-          this.dialogDelete = false;
-          this.$nextTick(() => {
-            this.editedItem = Object.assign({}, this.defaultItem);
-            this.editedIndex = -1;
-          });
-        },
-
-        save() {
-          if (this.editedIndex > -1) {
-            Object.assign(this.products[this.editedIndex], this.editedItem);
-          } else {
-            this.products.push(this.editedItem);
-          }
-          this.close();
-        }) }) });
+          }} }) });
       document.write(product_data)
   
