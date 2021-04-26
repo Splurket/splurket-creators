496284149bd1b12717041770ea17dbd586da6454
Dylan Clayton <dylanclayton3@gmail.com>
2021-04-26 14:03:31 -0400

    commit

--- vuescript.js
+++ vuescript.js
@@ -85,183 +85,76 @@ var firebaseConfig = {
       });
    });
 
-new Vue({
-  el: '#app',
-  data: () => ({
-    dialog: false,
-    dialogDelete: false,
-    pagination: {
-      rowsPerPage: 20,
-      sortBy: 'Date_Added' },
-
-    selected: [],
-    search: '',
-    isMobile: false,
-    headers: [
-    {
-      text: 'Dessert (100g serving)',
-      align: 'start',
-      sortable: false,
-      value: 'name' },
-
-    { text: 'Calories', value: 'calories' },
-    { text: 'Fat (g)', value: 'fat' },
-    { text: 'Carbs (g)', value: 'carbs' },
-    { text: 'Protein (g)', value: 'protein' },
-    { text: 'Actions', value: 'actions', sortable: false }],
-
-    selected: [],
-    editedIndex: -1,
-    editedItem: {
-      name: '',
-      calories: 0,
-      fat: 0,
-      carbs: 0,
-      protein: 0 },
-
-    defaultItem: {
-      name: '',
-      calories: 0,
-      fat: 0,
-      carbs: 0,
-      protein: 0 } }),
 
 
+    new Vue({
+      el: '#app',
+      data: () => ({
+        rowsPerPageItems: [10, 20, 30, 40],
+        pagination: {
+          rowsPerPage: 20,
+          sortBy: 'Date_Added' },
 
-  computed: {
-    formTitle() {
-      return this.editedIndex === -1 ? 'New Item' : 'Edit Item';
-    } },
+        selected: [],
+        search: '',
+        isMobile: false,
+        headers: [{
+          text: 'Product',
+          align: 'left',
+          value: 'name' },
 
+        {
+          text: 'Price',
+          value: 'Price' },
 
-  watch: {
-    dialog(val) {
-      val || this.close();
-    },
-    dialogDelete(val) {
-      val || this.closeDelete();
-    } },
+        {
+          text: 'Date Added',
+          value: 'Date_Added' },
 
+        {
+          text: 'Reviews',
+          value: 'reviews' },
 
-  created() {
-    this.initialize();
-  },
+        {
+          text: 'Purchases',
+          value: 'purchases' },
 
-  methods: {
-    initialize() {
-      this.desserts = [
-      {
-        name: 'Frozen Yogurt',
-        calories: 159,
-        fat: 6.0,
-        carbs: 24,
-        protein: 4.0 },
-
-      {
-        name: 'Ice cream sandwich',
-        calories: 237,
-        fat: 9.0,
-        carbs: 37,
-        protein: 4.3 },
-
-      {
-        name: 'Eclair',
-        calories: 262,
-        fat: 16.0,
-        carbs: 23,
-        protein: 6.0 },
-
-      {
-        name: 'Cupcake',
-        calories: 305,
-        fat: 3.7,
-        carbs: 67,
-        protein: 4.3 },
-
-      {
-        name: 'Gingerbread',
-        calories: 356,
-        fat: 16.0,
-        carbs: 49,
-        protein: 3.9 },
-
-      {
-        name: 'Jelly bean',
-        calories: 375,
-        fat: 0.0,
-        carbs: 94,
-        protein: 0.0 },
-
-      {
-        name: 'Lollipop',
-        calories: 392,
-        fat: 0.2,
-        carbs: 98,
-        protein: 0 },
-
-      {
-        name: 'Honeycomb',
-        calories: 408,
-        fat: 3.2,
-        carbs: 87,
-        protein: 6.5 },
-
-      {
-        name: 'Donut',
-        calories: 452,
-        fat: 25.0,
-        carbs: 51,
-        protein: 4.9 },
-
-      {
-        name: 'KitKat',
-        calories: 518,
-        fat: 26.0,
-        carbs: 65,
-        protein: 7 }];
+        {
+          text: 'Status',
+          value: 'status'},
+        {
+          text: 'Options',
+          value: 'options',
+          sortable: false }],
 
 
-    },
+        products: product_data}),
 
-    editItem(item) {
-      this.editedIndex = this.desserts.indexOf(item);
-      this.editedItem = Object.assign({}, item);
-      this.dialog = true;
-    },
 
-    deleteItem(item) {
-      this.editedIndex = this.desserts.indexOf(item);
-      this.editedItem = Object.assign({}, item);
-      this.dialogDelete = true;
-    },
 
-    deleteItemConfirm() {
-      this.desserts.splice(this.editedIndex, 1);
-      this.closeDelete();
-    },
 
-    close() {
-      this.dialog = false;
-      this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem);
-        this.editedIndex = -1;
-      });
-    },
-    
+      methods: {
+        onButtonClick(item) {
+          console.log('click on ' + item.no)
+        },
+        onResize() {
+          if (window.innerWidth < 769)
+          this.isMobile = true;else
 
-    closeDelete() {
-      this.dialogDelete = false;
-      this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem);
-        this.editedIndex = -1;
-      });
-    },
-
-    save() {
-      if (this.editedIndex > -1) {
-        Object.assign(this.desserts[this.editedIndex], this.editedItem);
-      } else {
-        this.desserts.push(this.editedItem);
-      }
-      this.close();
-    } } });
+          this.isMobile = false;
+        },
+        toggleAll() {
+          if (this.selected.length) this.selected = [];else
+          this.selected = this.desserts.slice();
+        },
+        changeSort(column) {
+          console.log(column);
+          if (this.pagination.sortBy === column) {
+            this.pagination.descending = !this.pagination.descending;
+          } else {
+            this.pagination.sortBy = column;
+            this.pagination.descending = false;
+          }
+        } } });
+    document.write(product_data)
+  
