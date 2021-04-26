c65c904ad621405d6c788f8fe8a4ff7b02d809b6
Dylan Clayton <dylanclayton3@gmail.com>
2021-04-26 13:14:28 -0400

    commit

--- vuescript.js
+++ vuescript.js
@@ -84,6 +84,7 @@ var firebaseConfig = {
         });
       });
    });
+
 new Vue({
   el: '#app',
   vuetify: new Vuetify(),
@@ -149,7 +150,79 @@ new Vue({
 
   methods: {
     initialize() {
-      this.desserts = product_data},
+      this.desserts = [
+      {
+        name: 'Frozen Yogurt',
+        calories: 159,
+        fat: 6.0,
+        carbs: 24,
+        protein: 4.0 },
+
+      {
+        name: 'Ice cream sandwich',
+        calories: 237,
+        fat: 9.0,
+        carbs: 37,
+        protein: 4.3 },
+
+      {
+        name: 'Eclair',
+        calories: 262,
+        fat: 16.0,
+        carbs: 23,
+        protein: 6.0 },
+
+      {
+        name: 'Cupcake',
+        calories: 305,
+        fat: 3.7,
+        carbs: 67,
+        protein: 4.3 },
+
+      {
+        name: 'Gingerbread',
+        calories: 356,
+        fat: 16.0,
+        carbs: 49,
+        protein: 3.9 },
+
+      {
+        name: 'Jelly bean',
+        calories: 375,
+        fat: 0.0,
+        carbs: 94,
+        protein: 0.0 },
+
+      {
+        name: 'Lollipop',
+        calories: 392,
+        fat: 0.2,
+        carbs: 98,
+        protein: 0 },
+
+      {
+        name: 'Honeycomb',
+        calories: 408,
+        fat: 3.2,
+        carbs: 87,
+        protein: 6.5 },
+
+      {
+        name: 'Donut',
+        calories: 452,
+        fat: 25.0,
+        carbs: 51,
+        protein: 4.9 },
+
+      {
+        name: 'KitKat',
+        calories: 518,
+        fat: 26.0,
+        carbs: 65,
+        protein: 7 }];
+
+
+    },
 
     editItem(item) {
       this.editedIndex = this.desserts.indexOf(item);
@@ -175,25 +248,7 @@ new Vue({
         this.editedIndex = -1;
       });
     },
-    onResize() {
-      if (window.innerWidth < 769)
-      this.isMobile = true;else
-
-      this.isMobile = false;
-    },
-    toggleAll() {
-      if (this.selected.length) this.selected = [];else
-      this.selected = this.desserts.slice();
-    },
-    changeSort(column) {
-      console.log(column);
-      if (this.pagination.sortBy === column) {
-        this.pagination.descending = !this.pagination.descending;
-      } else {
-        this.pagination.sortBy = column;
-        this.pagination.descending = false;
-      }
-    },
+    
 
     closeDelete() {
       this.dialogDelete = false;
