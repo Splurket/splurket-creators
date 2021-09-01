b347bcb70c0c882d68ceb60dddca93894a219430
DillonMercz <dylanclayton3@gmail.com>
2021-09-01 13:02:38 -0400

    commit

--- U466ywr363827dy.js
+++ U466ywr363827dy.js
@@ -41,18 +41,43 @@ var email1;
     db.collection("requests").doc('unclaimed').collection('products')
     .onSnapshot((snapshot) => {
         snapshot.docChanges().forEach((change) => {
-            if (change.type === "added") {
-                console.log("New city: ", change.doc.data());
+            if (change.type === "added" && change.doc.data().status === "New") {
+               db.collection("requests").doc('unclaimed').collection('products').doc(change.doc.id).update({
+               status: "read"
+            })
+            .then(() => {                //console.log("New city: ", change.doc.data());
+                var docid = change.doc.id
+                var doc = change.doc.data()
+                var push_data1 = {value: false, frontid: `front${docid}`, backid: `backfuck${docid}`, id: `${docid}`, name:`${doc.name}`, price: `${doc.price}`, date: `${doc.date}`, /*reviewsn: `${doc.product_reviewsn}`, creator: `${doc.product_creator}`, image: `${doc.product_cover}`, creatorpic: `${doc.product_creatorpic}`,*/ desc: `${doc.description}`, skills: `${doc.skills}`, permissions: `${doc.permissions}`, payment:`${doc.payment}`, category:`${doc.category}`, image: `${doc.cover}`}
+                if (product_data.includes('{')){
+                var push_data = ','+push_data1;
+                product_data.push(push_data)
+
+
+              }else{
+                var push_data = push_data1;
+                product_data.push(push_data)
+
+              }//document.write(product_data)
+              app.items = product_data
+            })
             }
-            if (change.type === "modified") {
-                console.log("Modified city: ", change.doc.data());
-            }
-            if (change.type === "removed") {
-                console.log("Removed city: ", change.doc.data());
+
+            //if (change.type === "modified") {
+                //console.log("Modified city: ", change.doc.data());
+            //}
+            if (change.type === "removed" && change.doc.data().status === "Swiped!") {
+                //console.log("Removed city: ", change.doc.data());
+                try{
+                document.getElementById('card' +change.doc.id).style.backgroundColor = "red"
+                document.getElementById('card1' +change.doc.id).style.backgroundColor = "red"
+              }catch(err){
+                console.log(err)
+              }
             }
         });
     });
-new Vue({
+app = new Vue({
   el: '#app',
   vuetify: new Vuetify(),
   data() {
@@ -151,16 +176,42 @@ new Vue({
                             mainContainer.appendChild(div2);
                         }
                     }*/
-
+  var doc
 function Snatch(firebase_id){
+
   var id = firebase_id.replace('snatchThis',"")
   console.log(id)
+  db.collection("requests").doc('unclaimed').collection('products').doc(id).update({
+   status: "Swiped!"
+  })
+  .then(() => {
+    removeDoc(id)
 
+  })
+}
+function removeDoc(id){
+    
   db.collection("requests").doc('unclaimed').collection('products').doc(id).get().then((doc) => {
-    console.log("Document data:", doc.data());
+    doc = doc
+    console.log("Document data:", doc.data())
+    console.log(doc)
+
+    db.collection("requests").doc('claimed').collection('products').doc(id).set(doc.data())
+      .then(() => {
+          db.collection("requests").doc('unclaimed').collection('products').doc(id).delete().then(() => {
+              console.log("Document successfully deleted!");
+          }).catch((error) => {
+              console.error("Error removing document: ", error);
+          });
+      })
+      .catch((error) => {
+          console.error("Error writing document: ", error);
+      });
+
   }).catch((error) => {
       console.log("Error getting document:", error);
   });
+
   
 }
 function hoverHandler(backside){
