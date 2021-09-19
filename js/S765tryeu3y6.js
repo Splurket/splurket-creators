e223ed1e718356409acb97c0ca75c3708b9a0095
DillonMercz <dylanclayton3@gmail.com>
2021-09-19 19:08:01 -0400

    commit

--- js/S765tryeu3y6.js
+++ js/S765tryeu3y6.js
@@ -38,7 +38,7 @@ var docRef = db.collection("requests").doc('unclaimed').collection('products').d
             //reviewsn1=parseFloat(doc.product_reviewsn)
             var push_data2 = {value: false, id: `${doc.product_id}`, name:`${doc.name}`, price: `${doc.price}`, date: `${doc.date}`, /*reviewsn: reviewsn1,*/ creator: `${doc.creator}`, image: `${doc.product_cover}`, creatorpic: `${doc.product_creatorpic}`, desc: `${doc.description}`, iframe: `./arctest?product=${doc.product_id}`, user: `${doc.user}`}
             db.collection("pubusers").doc(doc.user).get().then((doc) => {
-              console.log(doc.data())
+              document.getElementById('userPic').src = doc.data().image
             })
             if (products.includes('{')){
               var push_data = ','+push_data2;
@@ -111,6 +111,26 @@ new Vue({
     filteredKeys() {
       return this.keys.filter(key => key !== 'Name');
     } },
+    mounted() {
+    // Code that will run only after the
+    // entire view has been rendered
+          // hide the overlay when everything has loaded
+      // you could choose some other event, e.g. if you're loading
+      // data asynchronously, you could wait until that process returns
+
+    var timer = window.setInterval(function(){
+    var in1 = document.getElementById('app').innerHTML
+    if(in1.includes('No data available')) {
+     document.getElementById('loading-wrapper').style.display = "block";
+     document.getElementById('app').style.display = "none";
+     //console.log(in1)
+  }else{
+      document.getElementById('loading-wrapper').style.display = "none";
+     document.getElementById('app').style.display = "block";
+     window.clearInterval(timer);
+    }
+}, 100);
+},
 
   methods: {
     nextPage() {
@@ -122,3 +142,17 @@ new Vue({
     updateItemsPerPage(number) {
       this.itemsPerPage = number;
     } } });
+  var i = -1
+var tiparr = ["Get To Splurk!", "Become A Creator, Unlease Your Potential!", "Don't See What You Want In The Marketplace? Have It made Today!", "Have Some Files? Sell Them Here!", "Sell ANYTHING Digital!"]
+setInterval(function () {
+  if(i == tiparr.length-1){
+    document.getElementById("loading-tips").innerHTML = tiparr[0]
+        i=0; return;
+      }else if(i == undefined){
+    document.getElementById("loading-tips").innerHTML = tiparr[0]
+        i=0; return;
+    }else{
+      i++
+      document.getElementById("loading-tips").innerHTML = tiparr[i]
+    }
+}, 3000); // Execute something() 1 second later.
